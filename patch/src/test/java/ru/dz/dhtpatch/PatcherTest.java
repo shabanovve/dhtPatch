26ff44fe7a6d78483b01ee3b150d85ed05ba3d0f
Vladimir Shabanov <vladimir.shabanov@dz.ru>
2016-02-11 15:35:19 +0300

    Add byte read test

--- patch/src/test/java/ru/dz/dhtpatch/PatcherTest.java
+++ patch/src/test/java/ru/dz/dhtpatch/PatcherTest.java
@@ -34,24 +34,54 @@ public class PatcherTest {
     public void testReplaceWord() throws Exception {
         new Patcher().makePatch(path);
 
+        Path pathOftemp = Paths.get(fileName + ".tmp");
+        try (
+                FileChannel originChannel = FileChannel.open(path, StandardOpenOption.READ);
+                FileChannel tempChannel = FileChannel.open(pathOftemp, StandardOpenOption.READ);
+        ) {
+            assert originChannel.size() == tempChannel.size();
+
+            ByteBuffer originByteBuffer = ByteBuffer.allocate(10);
+            ByteBuffer tempByteBuffer = ByteBuffer.allocate(10);
+
+            int nReadOrgin, nReadTemp;
+            byte[] origin,temp;
+            do {
+                nReadOrgin = originChannel.read(originByteBuffer);
+                nReadTemp = originChannel.read(tempByteBuffer);
+
+                origin = originByteBuffer.array();
+                temp = tempByteBuffer.array();
+
+                for (int i = 0; i < originByteBuffer.array().length; i++) {
+                    if (origin[i] != temp[i]) {
+                        assert false;//String to place breakpoint
+                    }
+                }
+            } while (nReadOrgin > 0 && nReadTemp > 0);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 
     private Path createTestFile() throws IOException {
         Path path = Paths.get(fileName);
         Files.deleteIfExists(path);
         Files.createFile(path);
-        try (FileChannel fileChannel = FileChannel.open(path, StandardOpenOption.WRITE)){
-            fillWithStrings(fileChannel,0);
+        try (FileChannel fileChannel = FileChannel.open(path, StandardOpenOption.WRITE)) {
+            fillWithStrings(fileChannel, 0);
             fileChannel.write(ByteBuffer.wrap(Constant.PATTERN));
-            fillWithStrings(fileChannel,MAX);
-        } catch (Exception e){
+            fillWithStrings(fileChannel, MAX);
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return path;
     }
 
     private void fillWithStrings(FileChannel fileChannel, int start) throws IOException {
-        for (Integer i = start; i < start + MAX; i++){
+        for (Integer i = start; i < start + MAX; i++) {
             String text = i.toString() + "test ";
             fileChannel.write(ByteBuffer.wrap(text.getBytes()));
         }
