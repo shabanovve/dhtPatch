ea202ff72f6917bfacba576f03c255c7a6c096e9
Vladimir Shabanov <vladimir.shabanov@dz.ru>
2016-03-11 10:39:11 +0300

    Involve javafx

--- patch/src/main/java/ru/dz/dhtpatch/App.java
+++ patch/src/main/java/ru/dz/dhtpatch/App.java
@@ -1,12 +1,26 @@
 package ru.dz.dhtpatch;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
 import lombok.extern.java.Log;
 
 @Log
-public class App
+public class App extends Application
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        renderScene(primaryStage);
+
         System.out.println( "uTorrent DHT patch.");
         try{
             new Patcher().start();
@@ -15,5 +29,34 @@ public class App
             log.severe("Finished with error");
             log.severe(e.getMessage());
         }
+
+    }
+
+    private void renderScene(Stage primaryStage) {
+        primaryStage.setTitle("DHT-patch");
+
+        StackPane root = new StackPane();
+        primaryStage.setScene(new Scene(root, 600, 250));
+
+        TextField textField = new TextField ();
+        root.getChildren().add(textField);
+
+        addButton(root);
+
+        primaryStage.show();
+    }
+
+    private void addButton(StackPane root) {
+        Button btn = new Button();
+        btn.setText("Patch");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        root.getChildren().add(btn);
     }
 }
